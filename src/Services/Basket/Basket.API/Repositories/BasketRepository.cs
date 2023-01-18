481e233f3c560f94dabf64a58db2ec556ff557f2
os.fadldev@gmail.com <os.fadldev@gmail.com>
2023-01-18 23:39:19 +0300

    add basket controller

---
+++ src/Services/Basket/Basket.API/Repositories/BasketRepository.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+
+using Microsoft.Extensions.Caching.Distributed;
+
+using Newtonsoft.Json;
+
+using System.Text.Json.Serialization;
+
+namespace Basket.API.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCashe;
+        public BasketRepository(IDistributedCache redisCashe)
+        {
+            _redisCashe = redisCashe ?? throw new ArgumentNullException(nameof(redisCashe));
+        }
+        public async Task<ShoppingCart> GetBasket(string userName)
+        {
+            var basket = await _redisCashe.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCashe.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+        public async Task DeleteBasket(string userName)
+        {
+            await _redisCashe.RemoveAsync(userName);
+        }
+    }
+}
